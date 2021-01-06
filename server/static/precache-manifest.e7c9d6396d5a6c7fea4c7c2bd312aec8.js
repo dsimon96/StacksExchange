2fdd3fb1391648b8ee1f37932438912a13602470
David Simon <d@vidsimon.tech>
2021-01-06 01:40:53 -0800

    Check in build artifacts to ensure they are available to heroku build

---
+++ server/static/precache-manifest.e7c9d6396d5a6c7fea4c7c2bd312aec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7177705eafb712a9253cc00f25ae7d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b03ba358abfd8494c64",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "260d57c7cdd28614e630",
+    "url": "/static/js/2.1e82fbae.chunk.js"
+  },
+  {
+    "revision": "92bfebbc9186106791d506e48cb876d1",
+    "url": "/static/js/2.1e82fbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b03ba358abfd8494c64",
+    "url": "/static/js/main.19b66205.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
